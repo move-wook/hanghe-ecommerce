f3d80af49b6ff6a79fab4766b38e50c77a8b1b5e
DK2554 <ldu0215@gmail.com>
2025-01-16 00:26:32 +0900

    에러 발생시 log.error로 에러 남기기

--- src/main/java/kr/hhplus/be/server/support/ApiControllerAdvice.java
+++ src/main/java/kr/hhplus/be/server/support/ApiControllerAdvice.java
@@ -1,5 +1,7 @@
 package kr.hhplus.be.server.support;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,8 +9,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 class ApiControllerAdvice  {
 
+    private static final Logger log = LoggerFactory.getLogger(ApiControllerAdvice.class);
+
     @ExceptionHandler(HangHeaException.class)
     public ResponseEntity<CustomApiResponse<Void>> handleHangHeaException(HangHeaException e) {
+
+        log.error("Handled HangHeaException - Code: {}, Message: {}, HTTP Status: {}",
+                e.getCode(), e.getMessage(), e.getHttpStatus(), e);
+
         CustomApiResponse<Void> response = CustomApiResponse.fail(e.getCode(), e.getMessage());
         return ResponseEntity.status(e.getHttpStatus()).body(response);
     }
