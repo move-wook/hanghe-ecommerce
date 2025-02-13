6c30675fedb5b2d60f33c7ae805330a5441f9d70
move-wook <ldu0215@gmail.com>
2025-02-14 01:23:59 +0900

    주문 완료 facade 외부 API 호출 이벤트 처리

--- src/main/java/kr/hhplus/be/server/external/EcommerceDataPlatform.java
+++ src/main/java/kr/hhplus/be/server/external/EcommerceDataPlatform.java
@@ -1,10 +1,13 @@
 package kr.hhplus.be.server.external;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class EcommerceDataPlatform {
-    public boolean send(){
-        return true;
+    public void send(PaymentCompletedEvent eventListener) {
+        log.info("주문 이벤트 발생: 주문 ID = {}, {}, {}, {}",
+                eventListener.paymentId(), eventListener.orderId(), eventListener.totalAmount(), eventListener.userId());
     }
 }
---
+++ src/main/java/kr/hhplus/be/server/infra/external/PaymentCompletedListener.java
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.infra.external;
+
+import kr.hhplus.be.server.external.EcommerceDataPlatform;
+import kr.hhplus.be.server.external.PaymentCompletedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentCompletedListener {
+
+    private final EcommerceDataPlatform ecommerceDataPlatform;
+
+    @EventListener
+    public void handlePaymentCompleted(PaymentCompletedEvent event) {
+        ecommerceDataPlatform.send(event);
+    }
+}
